d2a97ef7b88be46050c50b16a4c6580366f92143
Chris Huber <chubes@extrachill.com>
2026-03-11 16:49:49 +0000

    refactor(architecture): remove more legacy project resolution paths

--- src/core/extension/execution.rs
+++ src/core/extension/execution.rs
@@ -344,7 +344,7 @@ fn resolve_extension_context(
             ExtensionScope::resolve_component_scope(extension, &loaded_project, component_id)?;
 
         if let Some(ref comp_id) = resolved_component_id {
-            component = Some(component::load(comp_id).map_err(|_| {
+            component = Some(project::resolve_project_component(&loaded_project, comp_id).map_err(|_| {
                 Error::config(format!(
                     "Component {} required by extension {} is not configured",
                     comp_id, &extension.id
