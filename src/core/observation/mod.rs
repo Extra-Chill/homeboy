ed079b4f8f7224cdcf35e6a8a12d20e82d010873
Chris Huber <chubes@extrachill.com>
2026-05-30 22:41:59 -0400

    refactor: remove no-op lint finding adapter

--- src/core/observation/mod.rs
+++ src/core/observation/mod.rs
@@ -17,11 +17,11 @@ pub use budget_findings::finding_records_from_budget;
 pub use records::{
     finding_record_from_audit, finding_record_from_lint, finding_records_from_annotation_file,
     finding_records_from_annotations_dir, finding_records_from_audit, finding_records_from_lint,
-    homeboy_finding_from_audit, homeboy_finding_from_lint, ArtifactCleanupCandidateRecord,
-    ArtifactCleanupFilter, ArtifactRecord, FindingListFilter, FindingRecord, NewFindingRecord,
-    NewRunRecord, NewRunRecordBuilder, NewTraceRunRecord, NewTraceRunRecordBuilder,
-    NewTraceSpanRecord, NewTraceSpanRecordBuilder, NewTriageItemRecord, RecordedHomeboyFinding,
-    RunListFilter, RunRecord, RunStatus, TraceRunRecord, TraceSpanRecord, TriageItemRecord,
+    homeboy_finding_from_audit, ArtifactCleanupCandidateRecord, ArtifactCleanupFilter,
+    ArtifactRecord, FindingListFilter, FindingRecord, NewFindingRecord, NewRunRecord,
+    NewRunRecordBuilder, NewTraceRunRecord, NewTraceRunRecordBuilder, NewTraceSpanRecord,
+    NewTraceSpanRecordBuilder, NewTriageItemRecord, RecordedHomeboyFinding, RunListFilter,
+    RunRecord, RunStatus, TraceRunRecord, TraceSpanRecord, TriageItemRecord,
     TriagePullRequestSignals,
 };
 pub use store::{
